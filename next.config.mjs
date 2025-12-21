5c4770c8e60987ded76b4352ba888603f5fbd671
Baskoro Cahhyo Heri Nugroho <baskorocahhyo@Baskoros-MacBook-Air.local>
2025-12-21 20:52:16 +0700

    fix: db

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['jsdom'],
+  serverExternalPackages: ['jsdom', 'isomorphic-dompurify'],
 
   images: {
     remotePatterns: [
