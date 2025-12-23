973f1e4f463b5a5433ce0be5d6fc0ee7fc955ffd
Baskoro Cahhyo Heri Nugroho <baskorocahhyo@Baskoros-MacBook-Air.local>
2025-12-23 10:10:07 +0700

    fix: sidebar

--- src/app/[novelSlug]/[chapterSlug]/ChapterClient.jsx
+++ src/app/[novelSlug]/[chapterSlug]/ChapterClient.jsx
@@ -201,8 +201,21 @@ export default function ChapterClient({
       <aside className={styles.rightSidebar}>
         <h3>Dukung Kami Yuk!</h3>
         <a href="https://saweria.co/SoogarGlyder" target="_blank" rel="noreferrer">
-          <img src="/saweria.png" alt="QR Code Saweria" width="200"/>
+          <img src="/saweria.png" alt="QR Code Saweria" width="100hv"/>
         </a>
+        <div style="max-height: 600px">
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4365395677457990"
+              crossorigin="anonymous"></script>
+          <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-client="ca-pub-4365395677457990"
+              data-ad-slot="4896743654"
+              data-ad-format="auto"
+              data-full-width-responsive="true"></ins>
+          <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        </div>
       </aside>
     </div>
   );
